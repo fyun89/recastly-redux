fe5d621369c0458cf1e75f83ac4bf6fbaeb12263
fyun89 <fyun89@gmail.com>
2018-07-11 23:29:40 -0700

    completed minimum req

--- src/components/App.js
+++ src/components/App.js
@@ -8,57 +8,64 @@ import changeVideo from '../actions/currentVideo.js';
 import changeVideoList from '../actions/videoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import store from '../store/store.js';
+import { connect } from 'react-redux'; //had to manually import this
+import handleVideoSearch from '../actions/search.js'; //had to manually import this
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      videos: [],
-      currentVideo: null
-    };
-  }
+  //   this.state = {
+  //     videos: [],
+  //     currentVideo: null
+  //   };
+  // }
 
   componentDidMount() {
-    this.getYouTubeVideos('react tutorials');
+    this.props.handleSearchInputChange('react tutorials');
   }
 
-  handleVideoListEntryTitleClick(video) {
-    this.setState({currentVideo: video});
-  }
+  // handleVideoListEntryTitleClick(video) {
+  //   this.setState({currentVideo: video});
+  // }
 
-  getYouTubeVideos(query) {
-    var options = {
-      key: this.props.API_KEY,
-      query: query
-    };
+  // getYouTubeVideos(query) {
+  //   var options = {
+  //     key: this.props.API_KEY,
+  //     query: query
+  //   };
 
-    this.props.searchYouTube(options, (videos) =>
-      this.setState({
-        videos: videos,
-        currentVideo: videos[0]
-      })
-    );
-  }
+  //   this.props.searchYouTube(options, (videos) =>
+  //     this.setState({
+  //       videos: videos,
+  //       currentVideo: videos[0]
+  //     })
+  //   );
+  // }
 
   //TODO: swap out the React components below for the container components
   //  you wrote in the 'containers' directory.
   render() {
     return (
       <div>
-        <Nav handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
+        <Nav handleSearchInputChange={this.props.handleSearchInputChange.bind(this)} />
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer />
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    handleSearchInputChange: (q) => dispatch(handleVideoSearch(q))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
